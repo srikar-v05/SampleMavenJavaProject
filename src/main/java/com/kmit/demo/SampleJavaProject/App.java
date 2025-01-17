c2c70a820b46281907b8be5e165049cedc836579
srikar-v05 <srikar.veluvali@gmail.com>
2025-01-17 20:59:54 +0530

    Added

---
+++ src/main/java/com/kmit/demo/SampleJavaProject/App.java
@@ -0,0 +1,10 @@
+package com.kmit.demo.SampleJavaProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is Srikar!");
+    }
+}
